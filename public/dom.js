896db8cddd20b8aecbabd90ff4a8315c5871d9fc
mFlaifel <msf-2oo7@hotmail.com>
2020-02-27 15:56:39 +0200

    update style relates #24

--- public/dom.js
+++ public/dom.js
@@ -13,15 +13,27 @@ const showData = (arr) => {
     const article = document.createElement('article');
     const artcileName = document.createElement('h3');
     const articleImg = document.createElement('img');
+    const ingeridentsTitle = document.createElement('h3');
     const ingeridents = document.createElement('p');
+    const preparationTitle = document.createElement('h3');
     const preparation = document.createElement('p');
+
     artcileName.textContent = el.name;
     articleImg.src = el.imgurl;
+    articleImg.alt = el.name;
+    ingeridentsTitle.textContent = 'Ingeridents: ';
+    ingeridentsTitle.classList.add('ing-title');
     ingeridents.textContent = el.ingredients;
+    ingeridents.classList.add('ing');
+    preparationTitle.textContent = 'Preparation: ';
+    preparationTitle.classList.add('prep-title');
     preparation.textContent = el.preparation;
+    preparation.classList.add('prep');
     article.appendChild(artcileName);
     article.appendChild(articleImg);
+    article.appendChild(ingeridentsTitle);
     article.appendChild(ingeridents);
+    article.appendChild(preparationTitle);
     article.appendChild(preparation);
     container.appendChild(article);
   });
